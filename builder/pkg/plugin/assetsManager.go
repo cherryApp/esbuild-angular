485858855849fd929e2a20c0c72f189959765a5f
Joe Cserko <j.cserko@gmail.com>
2022-11-16 14:28:53 +0100

    Merge and copy vendor.js

--- builder/pkg/plugin/assetsManager.go
+++ builder/pkg/plugin/assetsManager.go
@@ -3,12 +3,15 @@ package plugin
 import (
 	"os"
 	"path"
+  "fmt"
 
 	cp "github.com/otiai10/copy"
 
 	cpf "github.com/nmrshll/go-cp"
 
 	"github.com/evanw/esbuild/pkg/api"
+
+  "cherryApp/esbuild-angular/pkg/util"
 )
 
 func GetAssetManager(workingDir string, assets []interface{}, outPath string) api.Plugin {
@@ -16,6 +19,7 @@ func GetAssetManager(workingDir string, assets []interface{}, outPath string) ap
 		Name: "assetManager",
 		Setup: func(build api.PluginBuild) {
 
+      // Copy assets.
 			build.OnStart(func() (api.OnStartResult, error) {
         for _, v := range assets {
           assetPath := path.Join(workingDir, v.(string))
@@ -32,6 +36,46 @@ func GetAssetManager(workingDir string, assets []interface{}, outPath string) ap
             cpf.CopyFile(assetPath, dst)
           }
         }
+				return api.OnStartResult{}, nil
+			})
+
+      // Parse and copy css files.
+      build.OnStart(func() (api.OnStartResult, error) {
+        cssContent := ""
+
+        err := os.WriteFile(
+          path.Join(outPath, "main.css"),
+          []byte(cssContent),
+          0660,
+        )
+        util.Check(err)
+
+				return api.OnStartResult{}, nil
+			})
+
+      // Merge and copy .js files.
+      build.OnStart(func() (api.OnStartResult, error) {
+        vendorJSContent := ""
+
+        // Get scripts options from the angular.json
+        scripts := util.GetProjectOption("architect.build.options.scripts")
+        for _, v := range scripts.([]interface{}) {
+          scriptPath := path.Join(workingDir, v.(string));
+          scriptContent, err := os.ReadFile(scriptPath)
+          if err != nil {
+            fmt.Println("ERROR! Wrong filepath in: ", scriptPath)
+          } else {
+            vendorJSContent += string(scriptContent) + "\n\n"
+          }
+        }
+
+        err := os.WriteFile(
+          path.Join(outPath, "vendor.js"),
+          []byte(vendorJSContent),
+          0660,
+        )
+        util.Check(err)
+
 				return api.OnStartResult{}, nil
 			})
 
